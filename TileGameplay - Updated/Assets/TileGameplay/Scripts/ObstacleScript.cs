0fcfefe9d45454c9899788e6297bdcf23df4a040
ChrisDuzan <duzanc@email.sc.edu>
2013-11-15 11:00:51 -0500

    further obstacle changes

--- TileGameplay - Updated/Assets/TileGameplay/Scripts/ObstacleScript.cs
+++ TileGameplay - Updated/Assets/TileGameplay/Scripts/ObstacleScript.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class ObstacleScript : MonoBehaviour {
 	
-	public int length;				//if the object takes up more than one tile, otherwise equals 1
-	public int width;				//if the object takes up more than one tile, otherwise equals 1
+	//tiles taken up; ie. 1x1, 2x2, 2x1
+	public int length;				// x-direction; positive to the right from the origin
+	public int width;				// y-direction; positive in the up direction from the origin
+	
 	public int tileIndex;
 	private int tileBoardLength;
 	
@@ -14,15 +16,41 @@ public class ObstacleScript : MonoBehaviour {
 	void Start ()
 	{
 		levelManager = GameObject.Find("LevelManager");
+		tileBoardLength = levelManager.GetComponent<BoardManager>().tileBoardLength;
 		
 		string tileName;
 		tileName = "Tile" + tileIndex.ToString();
-		transform.position = GameObject.Find(tileName).transform.position; //move the character to its start position
-		levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().isOccupied = true;
-		levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().isOccupiedByObject = true;
-		levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().SetObject(gameObject);
-		
-		//***************add stuff if larger than 1x1
+		if(length == 1 && width == 1)
+		{
+			transform.position = GameObject.Find(tileName).transform.position; //move the object to its start position
+			levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().isOccupied = true;
+			levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().isOccupiedByObject = true;
+			levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().SetObject(gameObject);
+		}
+		else
+		{
+			Vector3 newPosition = GameObject.Find(tileName).transform.position;
+			
+			newPosition.x += (1/length);
+			newPosition.z += (1/width);
+			
+			transform.position = newPosition;
+			for(int i=0; i<length; i++)
+			{
+				levelManager.GetComponent<BoardManager>().tiles[tileIndex+(i*tileBoardLength)].GetComponent<GameTile>().isOccupied = true;
+				levelManager.GetComponent<BoardManager>().tiles[tileIndex+(i*tileBoardLength)].GetComponent<GameTile>().isOccupiedByObject = true;
+				levelManager.GetComponent<BoardManager>().tiles[tileIndex+(i*tileBoardLength)].GetComponent<GameTile>().SetObject(gameObject);
+			}
+			for(int i=0; i<width; i++)
+			{
+				levelManager.GetComponent<BoardManager>().tiles[tileIndex+i].GetComponent<GameTile>().isOccupied = true;
+				levelManager.GetComponent<BoardManager>().tiles[tileIndex+i].GetComponent<GameTile>().isOccupiedByObject = true;
+				levelManager.GetComponent<BoardManager>().tiles[tileIndex+i].GetComponent<GameTile>().SetObject(gameObject);
+			}
+//			levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().isOccupied = true;
+//			levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().isOccupiedByObject = true;
+//			levelManager.GetComponent<BoardManager>().tiles[tileIndex].GetComponent<GameTile>().SetObject(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
